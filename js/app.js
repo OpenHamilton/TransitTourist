b8fc0ac92a07b6035b1e262cad9694a0dccc92b3
Scott Elcomb <psema4@gmail.com>
2014-07-26 22:33:37 -0400

    cleanup, try to move the marker to user's location

--- js/app.js
+++ js/app.js
@@ -1,16 +1,51 @@
 // ORIGINAL SOURCE: http://openlayers.org/dev/examples/osm-marker-popup.js
+
 // References:
+//  http://trac.osgeo.org/openlayers/wiki/Documentation
 //  http://diveintohtml5.info/geolocation.html
 
-var map;
-var lon = -79.917452;
-var lat = 43.263626;
 
+// -- globals
+var map,
+    lon = -79.917452,
+    lat = 43.263626
+;
+
+
+// -- setup our location tracking
 function updatePosition(pos) {
-    console.log('in updatePosition()! got:', pos);
+    var overlay,
+        feature,
+        newLocation
+    ;
 
-    //lat = pos.coords.latitude;
-    //lon = pos.coords.longitude;
+    lat = pos.coords.latitude;
+    lon = pos.coords.longitude;
+
+    console.log('set position: %s, %s', lat, lon);
+
+    if (map) {
+        overlay = map.getLayersByName('poiOverlay')[0];
+
+        if (overlay) {
+            feature = overlay.getFeaturesByAttribute('id', 'user')[0];
+
+            if (feature) {
+                // not quite right, but not nearly as far off as with the transform below...
+                newLocation = new OpenLayers.Geometry.Point(lon, lat);
+
+                // marker ends up in Antarctica... we don't need to re-apply the transform?
+                //newLocation = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:3857');
+
+                feature.move(newLocation);
+
+                return;
+            }
+        }
+    }
+
+    // emit debug msg if !(feature||overlay||map)
+    console.log(map, overlay, feature);
 }
 
 function geoError(err) {
@@ -18,28 +53,29 @@ function geoError(err) {
 }
 
 function doUpdate() {
-    console.log('in doUpdate()!');
-
-    //navigator.geolocation.getCurrentPosition(
-    geoPosition.getCurrentPosition(
-        updatePosition,
-        geoError
-    );
+    geoPosition.getCurrentPosition(updatePosition, geoError);
 }
 
 function userInit() {
     if (geoPosition.init()) {
         doUpdate();
+
     } else {
         console.warn('no geolocation support');
     }
 }
 
+
+// -- setup our OSM map
 function init() {
     userInit();
 
-    // The overlay layer for our marker, with a simple diamond as symbol
-    var overlay = new OpenLayers.Layer.Vector('Overlay', {
+    var myLocation,
+        overlay,
+        popup
+    ;
+
+    overlay = new OpenLayers.Layer.Vector('poiOverlay', {
         styleMap: new OpenLayers.StyleMap({
             externalGraphic: './media/marker.png',
             graphicWidth: 20, graphicHeight: 24, graphicYOffset: -24,
@@ -49,32 +85,27 @@ function init() {
 
     // The location of our marker and popup. We usually think in geographic
     // coordinates ('EPSG:4326'), but the map is projected ('EPSG:3857').
-    //var myLocation = new OpenLayers.Geometry.Point(10.2, 48.9)
 
-    var myLocation = new OpenLayers.Geometry.Point(lon, lat)
-        .transform('EPSG:4326', 'EPSG:3857');
+    myLocation = new OpenLayers.Geometry.Point(lon, lat).transform('EPSG:4326', 'EPSG:3857');
 
-    // We add the marker with a tooltip text to the overlay
     overlay.addFeatures([
-        new OpenLayers.Feature.Vector(myLocation, {tooltip: 'Hackathon'})
+        new OpenLayers.Feature.Vector(myLocation, {id: 'user', tooltip: 'Hackathon'})
     ]);
 
-    // A popup with some information about our location
-    var popup = new OpenLayers.Popup.FramedCloud("Popup", 
+    popup = new OpenLayers.Popup.FramedCloud("Popup", 
         myLocation.getBounds().getCenterLonLat(), null,
         '<a target="_blank" href="http://openhamilton.ca/">Open Hamilton</a><br/>' +
         'HSR Hackathon', null,
         true // <-- true if we want a close (X) button, false otherwise
     );
 
-    // Finally we create the map
     map = new OpenLayers.Map({
-        div: "demoMap", projection: "EPSG:3857",
+        div: "baseMap",
+        projection: "EPSG:3857",
         layers: [new OpenLayers.Layer.OSM(), overlay],
         center: myLocation.getBounds().getCenterLonLat(), zoom: 15
     });
 
-    // and add the popup to it.
     map.addPopup(popup);
 }
 
